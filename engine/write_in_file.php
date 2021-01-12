58e7a4461407e46c92a783608e85e23eccac77dc
Nilarnab <59472120+Nilarnab@users.noreply.github.com>
2021-01-12 20:20:49 +0530

    Create write_in_file.php

---
+++ engine/write_in_file.php
@@ -0,0 +1,8 @@
+<?php
+$myfile = fopen("../text_files/test.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
