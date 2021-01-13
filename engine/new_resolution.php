07cee2143f218a2ebb32844b72345ee457e758e5
Nilarnab <59472120+Nilarnab@users.noreply.github.com>
2021-01-13 14:12:11 +0530

    Update new_resolution.php

--- engine/new_resolution.php
+++ engine/new_resolution.php
@@ -1,28 +1,89 @@
-<html>
-<head>
-<title>Online PHP Script Execution</title>
-</head>
-<body>
+<?php 
+    function remove_premises($premises)  
+    {
+        $new_premises = [];
+        
+        foreach ($premises as $p)
+        {
+            if (in_array($p, $new_premises) === false)
+            {
+                array_push($new_premises, $p);
+            }
+        }
+        
+        return $new_premises;
+        
+    }
+
+?>
+
+<?php function intra_premise($s)
+{
+    $a = explode("<OR>", $s);
+    
+    // echo "a \n";
+    // print_r($a);
+    $new = [];
+    $del = [];
+    
+    for ($i = 0; $i < count($a); $i++)
+    {
+        // echo "working on ".$a[$i]." \n";
+        if (strpos($a[$i], "<NEG>") !== false)
+        {
+            // echo "negation found \n";
+            array_push($del, $a[$i]);
+        }
+    }
+    
+    // print_r($del);
+    // echo "\n";
+    
+    for ($i = 0; $i < count($a); $i++)
+    {
+        if (in_array($a[$i], $del) === false && in_array("<NEG>".$a[$i], $del) === false && in_array($a[$i], $new) === false)
+        {
+            array_push($new, $a[$i]);
+        }
+    
+    }
+    
+    for ($i = 0; $i < count($del); $i++)
+    {
+        if (in_array(substr($del[$i], 5, strlen($del[$i])), $a) === false)
+        {
+            array_push($new, $del[$i]);
+        }
+    
+    }
+    
+    return implode("<OR>", $new);
+}
+    
+?>
+
 <?php 
 
     function resolution_util($premise)
     {
         
-        echo "resolution_util recieved ".sizeof($premise)."\n";
-        print_r($premise);
+        // echo "resolution_util recieved ".sizeof($premise)."\n";
+        // print_r($premise);
         for($i = 0; $i < sizeof($premise); $i++)
         {
-            echo $i;
+            // echo $i;
             if (empty($premise[$i]))
             {
                 // match found
-                echo "match found \n";
+                // echo "match found \n";
                 array_splice($premise, $i, 1);
+                
+                return resolution_util($premise);
             }
         }
         
-        echo "returned \n";
-        print_r($premise);
+        // echo "returned \n";
+        // print_r($premise);
         
         return $premise;
     }
@@ -34,18 +95,23 @@ function resolution($premise1, $premise2, $neg_literal)
 {
     // return resolved premises
     // $premise1 is negated
-    echo "data recieved in resolutnion \n";
-    print_r($premise1);
-    print_r($premise2);
-    echo $neg_literal."\n";
+    // echo "data recieved in resolutnion \n";
+    // print_r($premise1);
+    // echo "\n";
+    // print_r($premise2);
+    // echo "\n";
+    // echo $neg_literal."\n";
+    
+    $premise1 = explode("<OR>", $premise1);
+    $premise2 = explode("<OR>", $premise2);
     
     for($i = 0; $i < sizeof($premise1); $i++)
     {
-        echo "working on ".$premise1[$i]."\n";
+        // echo "working on ".$premise1[$i]."\n";
         if (strpos($premise1[$i], $neg_literal) !== false)
         {
             // match found
-            echo "match found \n";
+            // echo "match found \n";
             array_splice($premise1, $i, 1);
         }
     }
@@ -59,11 +125,46 @@ function resolution($premise1, $premise2, $neg_literal)
         }
     }
     
+
+    
+    
     $premise1 = resolution_util($premise1);
     $premise2 = resolution_util($premise2);
     
+      
     
-    return [implode($premise1, "<OR>"), implode($premise2, "<OR>")];
+    if (count($premise1) == 0)
+    {
+        
+        if (count($premise2) == 0)
+        {
+            
+            return [];
+        }
+        
+        else
+        {
+            return [implode("<OR>", $premise2)];
+        }
+        
+    }
+    else
+    {
+        if (empty($premise2))
+        {
+            
+            return [implode("<OR>", $premise1)];
+        }
+        
+        else
+        {
+            return [implode("<OR>", $premise1), implode("<OR>", $premise2)];
+        }
+        
+        
+    }
+    
+    
     
 }
 
@@ -72,228 +173,182 @@ function resolution($premise1, $premise2, $neg_literal)
 <?php 
 function simplify_by_resolution($premises)
 {
-    echo "primese recieved in main funcitno as \n";
-    print_r($premises);
-    $neg_found_once = false;
-    $appropriate_premise_found_once = false;
-    foreach ($premises as $premise)
-{
-  
-    // now we have to find the appropriate premise
-    $temp_array = explode("<OR>", $premise);
-    //print_r($temp_array);
     
-    // finding if any negated literal is present
-    $neg_pres = false;
-    $appropriate_premise_found = false;
-    $cnt = 0;
-    foreach ($temp_array as $literal)
+    echo "primeses recievied as \n";
+    print_r($premises);
+    
+    
+    $work = false;
+    $deletable = [];
+    // loop one
+    // for getting each premise
+    for($l1 = 0; $l1 < count($premises); $l1 ++)
     {
-        //echo "working on ".$literal." returning ". strpos($literal, "<NEG>");
-        if (strpos($literal, "<NEG>") !== false)
+        
+        // echo "premise 1 ".$premises[$l1]."\n";
+        //now exploding each premise
+        $candidate = explode("<OR>", $premises[$l1]);
+        
+        
+        // loop2 for each literal of a premise
+        for($l2 = 0; $l2 < count($candidate); $l2++)
         {
-            // echo "Negation is present ".$literal."<br>";
-            
-            
-            
-            // now an appropriate non negated form is to be found
-            // if it is not found, we should iterate to the next one
-            // we have to check if it is feasable or not
-            $findable = substr($literal, 5, strlen($literal));
-            for($itr = 0; $itr < count($premises); $itr++)
+            // now $candidate[$l2] has the words
+           
+            // if there is a negation, then we can proceed
+            if (strpos($candidate[$l2], "<NEG>") !== false)
             {
-                $search = explode("<OR>", $premises[$itr]);
+                // starting the loop now
+                // findig pair
                 
-                if (in_array($findable, $search))
+                $findable = substr($candidate[$l2], 5, strlen($candidate[$l2]));
+                // loop 3 for premieses
+                for($l3 = 0; $l3 < count($premises); $l3 ++)
                 {
-                    // negation is found
-                    $neg_pres = true;
-                    $neg_found_once = true;
-                    $negated_literal_index = $cnt;
-                    break;
+                    $poss_match = explode("<OR>", $premises[$l3]);
+                    //echo "checking in ".$premises[$l3]."\n";
+                    
+                    
+                    // now loop for poss match
+                    for($l4 = 0; $l4 < count($poss_match); $l4 ++)
+                    {
+                        
+                        
+                        // finding between $poss_match[$l4] == $findable
+                        //echo "finding between ".$poss_match[$l4]." and ".$findable."\n";
+                        
+                        if ($poss_match[$l4] == $findable)
+                        {
+                            // match found
+                            echo "match is found between ".$premises[$l1]." and ".$premises[$l3]."\n";
+                            
+                            array_push($deletable, $premises[$l1]);
+                            array_push($deletable, $premises[$l3]);
+                            
+                            
+                            // echo "omitting ".$findable."\n";
+                            $work = true;
+                            
+                            $ret = resolution($premises[$l1], $premises[$l3], $findable);
+                            
+                            
+                            
+                            // print_r($ret);
+                            
+                            if (count($ret) == 2)
+                            {
+                                $ins = $ret[0]."<OR>".$ret[1];
+                                
+                            }
+                            
+                            else if (count($ret) > 0)
+                            {
+                                $ins = $ret[0];
+                            }
+                            
+                            $ins = intra_premise($ins);
+                            
+                            if (count($ret) > 0)
+                            {
+                                array_push($premises, $ins);
+                            }
+                            
+                            
+                        }
+                        
+                        
+                        if ($work === true)
+                        {
+                            break;
+                        }
+                        
+                        
+                        
+                    }
+                    
+                    if ($work === true)
+                        {
+                            break;
+                        }
+                    
+                    
                 }
+                
+                
             }
             
-            if ($neg_pres === true && $neg_found_once === true)
-            {
-                break;
-            }
+            if ($work === true)
+                        {
+                            break;
+                        }
             
-            
-            
-            
-            //break;
             
         }
         
-        $cnt ++;
+        if ($work === true)
+                        {
+                            break;
+                        }
         
     }
     
-    if ($neg_pres === true)
+    
+    if ($work === true)
     {
-        $work = false;
-        // negation is already found
-        echo "NEGATION FOUND IN ".$literal."<br>";
-        // now an appropriate premise is to be found
-        // if it is not found, we should iterate to the next one
-        foreach ($premises as $other)
+        // deleting the previous ones
+        for($d1 = 0; $d1 < count($premises); $d1++)
         {
-            $temp_other = explode("<OR>", $other);
-            // now the literals are exploded
-            print_r($temp_other);
-            $cnt = 0;
-            foreach ($temp_other as $other_literal)
+            if ($premises[$d1] == $deletable[0])
             {
-                //echo "line no 44 <br>";
-                //print_r(explode("<NEG>", $literal));
-                
-                
-                if (strpos($other_literal, $findable) !== false && strpos($other_literal, $literal) === false)
-                {
-                    // appropriate match for resolution found
-                    
-                    echo "APPROPRIATE RSOLUTOIN FOUND HERE\n";
-                    $appropriate_premise_found = true;
-                    $appropriate_premise_found_once = true;
-                    
-                    // now matching should be done
-                    $param1 = implode($temp_array, "<OR>");
-                    $param2 = implode($temp_other, "<OR>");
-                    echo "candidates found as ".$param1." ".$param2." for ".$literal."\n";
-                    
-                    $resolved_val = resolution($temp_array, $temp_other, explode("<NEG>", $literal)[1]);
-                    
-                    
-                    // echo "DELETING ".$premises[$negated_literal_index]." AND ".$premises[$cnt]." \n";
-                    //array_splice($premises, $negated_literal_index, 1);
-                    //array_splice($premises, $cnt - 1, 1);
-                    
-                    echo "resoved value ".$resolved_val[0]." and ".$resolved_val[1]." ".strlen($resolved_val[0])."\n";
-                    
-                    $push = true;
-                    if ((strlen($resolved_val[0]) != 0) && (strlen($resolved_val[1])!=0))
-                    {
-                        echo "cond 1\n";
-                        $new_premise = $resolved_val[0]."<OR>".$resolved_val[1];
-                    }
-                    else
-                    {
-                        echo "cond 2\n";
-                        if (!empty($resolved_val[0]))
-                        {
-                            echo "cond 2.1\n";
-                            $new_premise = $resolved_val[0];
-                        }
-                        
-                        else if (!empty($resolved_val[1]))
-                        {
-                            echo "cond 2.2\n";
-                            $new_premise = $resolved_val[1];
-                        }
-                        
-                        else
-                        {
-                            $push = false;
-                        }
-                            
-                    }
-                    
-                    // $new_premise = $resolved_val[0]."<OR>".$resolved_val[1];
-                    if ($push)
-                    {
-                        array_push($premises, $new_premise);   
-                    }
-                    
-                    print_r($resolved_val);
-                    
-                    // making the new array
-                    $new_prm = [];
-                    
-                    
-                    print_r($premises);
-                    
-                    echo "not adding ".$param1." ".$param2."\n";
-                    foreach ($premises as $prem)
-                    {
-                        echo "ATTEMPTING TO ADD ".$prem." \n";
-                        if ($prem == $param1 or $prem == $param2)
-                        {
-                            
-                        }
-                        else
-                        {
-                            if (!in_array($prem, $new_prm))
-                            {
-                                array_push($new_prm, $prem);
-                            }
-                            
-                        }
-                    }
-                    
-                    // now adding the new premises
-                    if (!in_array($new_premise, $new_prm))
-                    {
-                        array_push($new_prm, $new_premise);
-                    }
-                    
-                    
-                    
-                    $premises = $new_prm;
-                    
-                    echo "NEW PREMISES FOUND AS \n";
-                    print_r($premises);
-                    
-                    
-                    // now the the param1 and param 2 are to be deleted
-                    $work = true;
-                    break;
-                }
-                
-                $cnt ++;
-                
-                
+                // echo "deleting ".$deletable[0]."\n";
+                array_splice($premises, $d1, 1);
+                break;
             }
         }
-        if ($work === true)
-                {
-                    break;
-                }
-        
+    
+        for($d2 = 0; $d2 < count($premises); $d2++)
+        {
+            if ($premises[$d2] == $deletable[1])
+            {
+                //echo "deleting ".$deletable[1]."\n";
+                array_splice($premises, $d2, 1);
+                break;
+            }
+        }
+    }
+    
+    $premises = resolution_util($premises);
+    $premises = remove_premises($premises);
+    
+    if ($work === true)
+    {
+        return simplify_by_resolution($premises);
     }
     else
     {
-        continue;
+        return $premises;
     }
-}
-
-echo "PROCESS COMPLETE****************************************\n";
-
-print_r($premises);
-
-if (($neg_found_once && $appropriate_premise_found_once) && count($premises) > 1)
-{
-    return simplify_by_resolution($premises);
-}
-else
-{
-    return $premises;
-}
+    
 }
 
 ?>
 
 <?php
-$premises = ["A<OR>B<OR><NEG>C", "<NEG>A<OR>C"];
+$premises = ["A<OR>B<OR>C<OR>D", "<NEG>A<OR>B", "<NEG>D<OR>B"];
 $premises = simplify_by_resolution($premises);
 
 echo "fully resolved premises \n";
 
 print_r($premises);
 
+// $s = "<NEG>C";
+// echo intra_premise($s)."\n";
+
+
+// echo intra_premise("<NEG>C<OR>B<OR>C<OR><NEG>B");
+// echo "\n";
+
 
 
 
 ?>
-</body>
-</html>
