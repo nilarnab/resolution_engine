c7857b7566f10c4ccb41af14397259c1692c6ae3
kunallverma007 <58910303+kunallverma007@users.noreply.github.com>
2021-01-14 13:26:15 +0530

    Update BIT.py

--- engine/BIT.py
+++ engine/BIT.py
@@ -23,3 +23,17 @@ dictionary
  xor:^
  not : ~
 '''
+import re
+
+myfile = "C:\\Users\hp\Downloads\College-Website-main\College-Website-main\lat\Images\cs.txt"
+
+with open(myfile, "r+") as f:
+    content = f.read()
+    cmds = content.split('\n')
+    for i in range(len(cmds)):
+        cmds[i] = toCnf(cmds[i])
+    f.seek(0)
+    f.writelines(cmds)
+    f.truncate()
+
+
