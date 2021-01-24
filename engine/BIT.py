8019fb18cd7c1fdb7ec5887c2a9618d05e1f35d6
kunallverma007 <58910303+kunallverma007@users.noreply.github.com>
2021-01-24 13:19:52 +0530

    Update BIT.py

--- engine/BIT.py
+++ engine/BIT.py
@@ -5,35 +5,33 @@ def toCnf(s):
     try:
         z=simplify_logic(s)
         x=to_cnf(z)
-        return x
+        return str(x)
     except:
         return 'this is wrong format'
 '''
 directions for use:
-
 dictionary
-
  or : Or
  and :And
+ +
  not:Not
  xor:Xor
+ implies :Implies(a,b)
 dictionary
  or : |
  and : &
  xor:^
  not : ~
+ implies : >>
 '''
-import re
 
 myfile = "C:\\Users\hp\Downloads\College-Website-main\College-Website-main\lat\Images\cs.txt"
 
 with open(myfile, "r+") as f:
     content = f.read()
-    cmds = content.split('\n')
+    cmds = content.split('<OR>')
     for i in range(len(cmds)):
-        cmds[i] = toCnf(cmds[i])
+        cmds[i] = toCnf(cmds[i]) + '\n'
     f.seek(0)
     f.writelines(cmds)
     f.truncate()
-
-
